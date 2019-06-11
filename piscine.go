1a72dc8f098dd6d45129676767eae531f303ce57
Opeyemi19 <sodikfatayo@gmail.com>
2019-06-11 13:15:57 +0000

    Autre

--- piscine.go
+++ piscine.go
@@ -1,22 +1,6 @@
 package piscine
 
-import (
-	"strconv"
-)
-
-// Atoi ...
-func Atoi(s string) int {
-
-	// if s == ParseInt() {
-	// 	// return 0
-	// }
-
-	if S, err := strconv.Atoi(s); err == nil {
-		// fmt.Printf("%v", S)
-		// fmt.Println(S)
-		return S
-	}
-
-	return 0
-
-}
+//Countdown ...
+// func Countdown(n int) {
+// 	fmt.Println(n)
+// }
