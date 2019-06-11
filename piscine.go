df5d32f34a7bb2e8ca61137654d24c0b699ed477
Opeyemi19 <sodikfatayo@gmail.com>
2019-06-11 05:38:33 +0000

    Decompte

--- piscine.go
+++ piscine.go
@@ -2,11 +2,7 @@ package piscine
 
 import "fmt"
 
-//IsNegative ...
-func IsNegative(nb int) {
-	if nb < 0 {
-		fmt.Println("T")
-	} else {
-		fmt.Println("F")
-	}
+//Countdown ...
+func Countdown(n int) {
+	fmt.Println(n)
 }
